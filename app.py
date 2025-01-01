f56fcbd3df1be63248a71155dd5f4b68339263c1
yu-working <tsaiyuforwork@gmail.com>
2025-01-01 22:17:22 +0800

    Update app.py
    RAG

--- app.py
+++ app.py
@@ -15,12 +15,13 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from googlesearch import search
 from lxml.html import fromstring
+import numpy as np
 
 #===
 def yahoo_news(keyword):
     yh_news = []
-    keyword += "yahoo"
-    for j in search(keyword, stop=3, pause=2.0, lang='zh-tw'):
+    yahoo_keyword = keyword + "yahoo"
+    for j in search(yahoo_keyword, stop=3, pause=2.0, lang='zh-tw'):
             print(j) #可忽略
             r = requests.get(j)
             try:
@@ -30,16 +31,26 @@ def yahoo_news(keyword):
             title = tree.findtext('.//title')
             if title == '403 Forbidden':
                 title = ""
-            print(title)
             soup = BeautifulSoup(r.text,"html.parser")
             sel = soup.select(f"div.caas-body")
             for s in sel:
                 print(s.text)
             yh_news.append([title, sel])
     return yh_news
+#===last newest
+def last_news():
+    last = []
+    r = requests.get("https://tsna.com/baseball") #將網頁資料GET下來
+    soup = BeautifulSoup(r.text,"html.parser") #將網頁資料以html.parser
+    for i in range(10):
+        sel = soup.select(f"#__layout > section > main > div > section > div.max-w-screen-xl.mx-auto > div.news-container > div.news__left > div.news__left__news > a:nth-child({i+1}) > div.news__content > div.main-content > div.news__content__title.news__content__title--pc") #取HTML標中的 <div class="title"></div> 中的<a>標籤存入sel
+        for s in sel:
+            print(s.text)
+            last.append(s.text)
+    return last
 #===
 
-def ptt_search(ptt_url="https://www.ptt.cc/bbs/FAPL/index.html"):
+def ptt_search(ptt_url="https://www.ptt.cc/bbs/Baseball/index.html"):
     r = requests.get(ptt_url) #將網頁資料GET下來
     soup = BeautifulSoup(r.text,"html.parser") #將網頁資料以html.parser
     sel = soup.select("div.title a") #取HTML標中的 <div class="title"></div> 中的<a>標籤存入sel
@@ -71,33 +82,55 @@ def ptt_search(ptt_url="https://www.ptt.cc/bbs/FAPL/index.html"):
     return ptt_df
 
 his = None
-ptt_url = "https://www.ptt.cc/bbs/FAPL/index.html"
+ptt_url = "https://www.ptt.cc/bbs/Baseball/index.html"
 
 st.session_state.chat_history = pd.read_csv('user_inputs.csv') if 'user_inputs.csv' in os.listdir() else pd.DataFrame(columns=['Uesr', 'AI'])
 now = datetime.now().strftime('%Y-%m-%d %H:%M') #colab
 st.session_state.ptt_df = pd.read_csv('ptt_news.csv') if 'ptt_news.csv' in os.listdir() else ptt_search()
 
 
-def RAG(Ncsv='N.csv',Mcsv='M.csv'):
-    import ast  # for converting embeddings saved as strings back to arrays
-    from scipy import spatial  # for calculating vector similarities for search
-    Ncsv_dir = Ncsv
-    Mcsv_dir = Mcsv
-    # models
+def RAG(text_list,user_message):
+    # 創建一個空列表來存儲嵌入
+    embeddings = []
+    # 為每個句子生成嵌入
     api_key = os.getenv("OPENAI_API_KEY")
     client = OpenAI(api_key=api_key)
-    EMBEDDING_MODEL = "text-embedding-ada-002"
-    GPT_MODEL = "gpt-4o"
+    for sentence in text_list:
+        response = client.embeddings.create(
+            input=sentence,
+            model="text-embedding-3-small"
+        )
+        embeddings.append(response.data[0].embedding)
+    # 將文本和嵌入存入資料框
+    df = pd.DataFrame({
+        "text": text_list,
+        "ada_embedding": embeddings
+    })
+    #將使用者提問嵌入
+    user_message_embedding = client.embeddings.create(
+            input=user_message,
+            model="text-embedding-3-small"
+        )
+    new_sentence_embedding = np.array(user_message_embedding.data[0].embedding)
+
+    # 歸一化現有的嵌入向量
+    embeddings = np.array(df["ada_embedding"].tolist())
+    normalized_embeddings = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
+
+    # 歸一化新句子的嵌入向量
+    new_sentence_embedding_normalized = new_sentence_embedding / np.linalg.norm(new_sentence_embedding)
+
+    # 計算新句子與所有文本的餘弦相似度
+    cosine_similarities = np.dot(normalized_embeddings, new_sentence_embedding_normalized)
+
+    # 找到最相似的文本
+    max_index = np.argmax(cosine_similarities)
+
+    # 提取最相似的文本
+    most_similar_text = df.iloc[max_index]["text"]
     
-    #get data
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.max_colwidth', None)
-    Ndata = pd.read_csv(Ncsv_dir, usecols=['Title', 'Content'])
-    Mdata = pd.read_csv(Mcsv_dir)
-    # 顯示最後 5 筆資料
-    Mdata = Mdata.tail(50)
-    return Ndata,Mdata
+    rag_result = most_similar_text
+    return rag_result
 
 #連結至LLM
 def get_answer(user_message,his,ptt_df,
@@ -118,7 +151,6 @@ def get_answer(user_message,his,ptt_df,
             st.session_state.ptt_df = ptt_search(ptt_url)
         else:
             st.session_state.ptt_df = ptt_search(ptt_url)
-        Ndata,Mdata = RAG()
 
         # 發送請求到 OpenAI API，並獲取回應
         keyword_maker = client.chat.completions.create(
@@ -126,7 +158,7 @@ def get_answer(user_message,his,ptt_df,
             messages =  [
             {  # prompt
                 'role':'system',
-                'content': "將使用者提問轉換為搜尋引擎用的關鍵字"
+                'content': "將使用者提問轉換為用於搜尋引擎的關鍵字"
             },
             {
                 'role':'user',
@@ -142,6 +174,9 @@ def get_answer(user_message,his,ptt_df,
                 keyword += chunk.choices[0].delta.content
         #st.title(keyword) keyword檢視
         yh_news = yahoo_news(keyword)
+        last = last_news()
+        text_list = last
+        rag_result = RAG(text_list,user_message)
         #--------------------------------------------------------------------------#
         res = ""
         stream = client.chat.completions.create(
@@ -150,10 +185,11 @@ def get_answer(user_message,his,ptt_df,
             {  # prompt
                 'role':'system',
                 'content': f"""
-                你是一個經常觀看足球賽事的人，請按照使用者的提問，根據當下時間{now}以及現有資料，使用繁體中文進行簡潔的回覆，不得超過三個段落，每個段落也需簡短。
-                當使用者以簡稱詢問時，請使用完整名稱如:英格蘭超級足球聯賽、西班牙甲級足球聯賽、馬德里競技俱樂部、皇家馬德里足球俱樂部等進行回覆，
+                你是一個經常觀看棒球賽事的人，請按照使用者的提問，根據當下時間{now}以及現有資料，使用繁體中文進行簡潔的回覆，不得超過三個段落，每個段落也需簡短。
+                當使用者詢問時，請使用完整名稱如:世界棒球12強賽、中華臺北、大谷翔平等進行回覆，
                 並且在全稱後面加上英文原文作為註記。
-                請先查看歷史資料庫中{Ndata}、{Mdata}、歷史對話紀錄{his}、論壇'PTT'上近期的資料{ptt_df}、yahoo新聞網站上{yh_news}有無相關資料。
+                請最優先參考TSNA新聞網站{rag_result}有無相關資料
+                其次查看歷史對話紀錄{his}、論壇'PTT'上近期的資料{ptt_df}、yahoo新聞網站上{yh_news}。
                 當你進行預測時，需有準確的比分以提供使用者參考，並且提及球員表現作為分析依據。
                 當你進行知識介紹時，可以簡單介紹後結合新聞介紹
                 當你提到球員表現時，需準確的提出相關的比賽。
@@ -177,15 +213,15 @@ def get_answer(user_message,his,ptt_df,
 
 def main():
     global new_entry
-    st.title('大語言模型應用於足球運動賽事分析')
-    st.subheader("歡迎來到FanFormation！")
-    st.write("一個專為運動設計的大型語言模型工具，FanFormation可能會發生錯誤。請查核重要資訊。")
+    st.title('歡迎來到FanFormation！')
+    st.subheader("一個專為運動設計的大型語言模型工具")
+    st.write("FanFormation可能會發生錯誤。請查核重要資訊。")
     #NEW
     default_text = "請輸入你的問題..."
-    preset_text_1 = "預測日本對上印尼的比分。"
-    preset_text_2 = "2016年的西甲冠軍是誰?"
-    preset_text_3 = "英超目前最新的賽制?"
-    preset_text_4 = "最近世界盃資格賽怎麼樣?"
+    preset_text_1 = "預測中華臺北對上日本的比分。"
+    preset_text_2 = "2024年世界棒球12強賽的冠軍是誰?"
+    preset_text_3 = "目前世界棒球12強賽最新的賽制?"
+    preset_text_4 = "賴清德在中華臺北取得12強冠軍後有何表示?"
     preset_text_5 = "請為我組建一支最強的隊伍?"
 
     if 'user_message' not in st.session_state:
@@ -237,15 +273,30 @@ def main():
         new_entry.to_csv('user_inputs.csv', mode='a', header=not os.path.exists('user_inputs.csv'), index=False)
         
         st.success('complete!')
-        
+        bg_color = "#f0f8ff"
         # get history
         for index, row in st.session_state.chat_history.iloc[::-1].iterrows():
             user = str(row['User'])
             text = str(row['AI'])
             
             #print history
-            st.markdown("User : " + user, unsafe_allow_html=True)
-            st.markdown("FanFormation : "+ text, unsafe_allow_html=True)
+            st.markdown(            
+                f"""
+                <div style="text-align: right;; background-color: {bg_color}; padding: 10px; border-radius: 5px;">
+                    {user} : User  
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
+            st.write("")
+            st.markdown(            
+                f"""
+                <div style="text-align: left;; background-color: {bg_color}; padding: 10px; border-radius: 5px;">
+                    FanFormation : {text}  
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
             st.markdown("---")
 
 if __name__ == "__main__":
